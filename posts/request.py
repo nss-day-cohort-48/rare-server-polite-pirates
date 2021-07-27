8abb4252c833964bb3c8cccaf358cb111c6779d5
Jaclyn French <jaclynmariefrench@gmail.com>
2021-07-27 15:14:49 -0500

    have some formatting issues on the Delete function in the requests module

--- posts/request.py
+++ posts/request.py
@@ -7,7 +7,7 @@ def get_all_posts():
     with sqlite3.connect("./rare.db") as conn:
 
         conn.row_factory = sqlite3.Row
-        db_cursor = conn.cursor() 
+        db_cursor = conn.cursor()
 
         db_cursor.execute("""
         SELECT
@@ -29,7 +29,7 @@ def get_all_posts():
         for row in dataset:
 
             post = Post(row['id'], row['user_id'], row['category_id'], row['title'], row['publication_date'],
-                            row['image_url'] ,row['content'], row['approved'])
+                        row['image_url'], row['content'], row['approved'])
 
             posts.append(post.__dict__)
 
@@ -54,7 +54,9 @@ def create_post(new_post):
 
     return json.dumps(new_post)
 
+
 def delete_post(id):
+    """
         DELETE FROM posts
         WHERE id = ?
-        """, (id, ))
+    """, (id, )
